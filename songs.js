220b9dc1b4676c25bfd924cc829e868513551dca
Ahmed Ansari <ahmedansari153@gmail.com>
2016-01-13 11:05:25 -0600

    Ayyyyy

--- songs.js
+++ songs.js
@@ -7,6 +7,11 @@ songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album App
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 songs[songs.length] = "An Ocean and a Rock > by Lisa Hannigan on the album Sea Sew";
 
+
 for (var i = 0; i < songs.length; i++) {
-	songs[i]
+	songs[i] = songs[i].replace(/[^a-zA-Z&>\s]/g, "")
+	songs[i] = songs[i].replace(/>/g, "-")
 };
+console.log("Replaced > with -");
+
+alert(songs)
